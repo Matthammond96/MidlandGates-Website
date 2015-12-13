50152d162ce1124e8df5652224dff1e8f7bd4ce2
Matthammond96 <i7699585@bournemouth.ac.uk>
2015-12-13 19:18:24 +0000

    Carousel Sizing Fixed
    Fixed the carousel size

--- js/homePageOnLoad.js
+++ js/homePageOnLoad.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     var imageSize = $('#topContainer').height();
  
-    $('#headerRow').css('margin-top', imageSize * 0.5);
+    $('#headerRow').css('padding-top', imageSize * 0.5);
     $(".contentContainer").css("min-height",$(window).height())
+    $("#homeCarousel").css("height", $(window).height());
+    $(".item").css("height", $(window).height());
 })
