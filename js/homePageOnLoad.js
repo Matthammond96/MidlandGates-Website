00e1cfde580a8d58f66138b793144bddb373cdf3
Matthammond96 <i7699585@bournemouth.ac.uk>
2015-12-13 23:41:16 +0000

    Resizing Issues Fixed
    Add Jquery to update header size when page is re-sized

--- js/homePageOnLoad.js
+++ js/homePageOnLoad.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-    var imageSize = $('.item').height();
+    var imageSize = $('#myCarousel').height();
+    console.log(imageSize * 0.2);
  
-    $('#headerRow').css('padding-top', "100px");
+    $('#headerRow').css('padding-top', imageSize * 0.2);
     $(".contentContainer").css("min-height", imageSize);
     $("#homeCarousel").css("height", $(window).height());
     //$(".item").css("height", $(window).height());
+})
+
+$(window).resize(function() {
+    var newSizing = $('#myCarousel').height();
+    console.log('new height: ' + newSizing);
+    
+    $(".contentContainer").css("min-height", newSizing);
+    
+    if ($(window).width() >= 1000) {
+        $('#headerRow').css('padding-top', newSizing * 0.2);
+        $(".contentContainer").css("min-height", newSizing);
+    } else {
+        $('#headerRow').css('padding-top', newSizing * 0.05);
+    }
 })
