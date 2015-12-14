b34a4f87596bdc1f4700be9ed99709dc89d08fef
zacawee <zac.mcgarry@hotmail.co.uk>
2015-12-14 20:45:42 +0000

    Added jquery scroll

---
+++ js/linkscroll.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".animatedScroll").click(function(){
+       var linkattr = $(this).attr("data-link");
+        
+        $("html, body").animate({
+            scrollTop: $(linkattr).position().top
+        }, 300);
+        
+    })
+    
+    
+})
+                  
