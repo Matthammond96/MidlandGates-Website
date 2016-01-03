f07b3a48f5759804e000963a5830584204600bb0
zacawee <zac.mcgarry@hotmail.co.uk>
2016-01-03 14:55:27 +0000

    Started to add meta tags and data READ DESC
    Added privacy policy and edited mid footer col title.
    Also changed titles on all pages to correct margin and size.

--- contact-us.php
+++ contact-us.php
@@ -76,7 +76,15 @@
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-   
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-71923058-1', 'auto');
+    ga('send', 'pageview');
+    </script>
 </head>
 <body data-navSetting="contact-us"> 
     
@@ -90,13 +98,13 @@
              <!--   <div class="row" id="headerRow"> -->
                     
   <div class="screenSize">
-    <div class="container contentContainer" id="topRow">
+    <div class="container contentContainer" id="">
     
         <div class="row">
       
         <div class="col-md-10 col-md-offset-1 center" id="">
         
-        <h1>Midland Gates</h1>
+        <h1 class="title center">Midland Gates</h1>
         <h2>Contact Us</h2>
         <p class="lead">Wooden Driveway Gates Hand Made To Order In The Heart Of The National Forest.</p>
             
